42232b039604a4c5c80a878c7da23fa0bacbe01a
SovietG <jchulavatr@gmail.com>
2025-05-27 21:47:40 -0400

    removed auto scaling

--- main.tf
+++ main.tf
@@ -31,19 +31,17 @@ module "blog_vpc" {
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.3.0"
-  # insert the 1 required variable here
+  version = "6.5.2"
 
-  name     = "blog"
-  min_size = 1
-  max_size = 2
+  name = "blog"
 
-  vpc_zone_identifier                   = module.blog_vpc.public_subnets
-  autoscaling_group_target_group_arns   = module.blog_alb.target_group_arns
-  security_groups                       = [module.blog_sg.security_group_id]
-
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
